379945396c5cf6c3112d3cc58d924814b60a9081
00chamara <116842342+00chamara@users.noreply.github.com>
2022-10-28 01:21:24 +0530

    Update Hacktomberfest_2022_PHP_Tast04.php

--- Hacktomberfest_2022_PHP_Tast04.php
+++ Hacktomberfest_2022_PHP_Tast04.php
@@ -1,16 +1,22 @@
 <?php
+
+
 class Fruit {
-  public $name;
-  public $color
-
-  function __construct($name) {
-    $this->name = $name;
-  }
-  function get_name() {
-    return this->name;
-  }
+  
+  
+ public $name;
+ public $color;
+ function __construct($name) {
+   
+ $this->name = $name;
+   
+ }
+  
+  
+ function get_name() {
+ return $this->name;
+ }
 }
-
 $apple = new Fruit("Apple");
-echo $apple->get_name()
->
+echo $apple->get_name();
+?>
